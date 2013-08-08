f62629625a2805d425fccc2abfe47db0ad02956d
Ryan Kaplan <ryankap@gmail.com>
2013-08-07 20:59:21 -0700

    Add send_email script

---
+++ app/lib/send_email.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# From: http://segfault.in/2010/12/sending-gmail-from-python/
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 587
+
+sender = 'bobmahoney1234@gmail.com'
+password = 'this_is_not_the_real_password_because_were_hosting_this_on_github_and_that_would_be_cray'
+subject = 'Testing'
+message = """
+<html>
+    <body>
+        <div class="container" style="background-color: #4688f1; color: white; border-radius: 3px; padding: 15px; text-align: center;">
+
+            <div class="kudos header" style="font-size: 25px;">
+                Someone sent you Kudos...
+            </div>
+
+            <div class="kudos message" style="width: 320px; margin: 15px auto auto auto; color: white;">
+                Soleio is a cool guy! He will make this email look way more awesome than it is right now.
+                At the very least, it is currently way too blue and the font is the default ugly GMail font.
+            </div>
+    </body>
+</html>
+"""
+
+def send_email_to(recipient):
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg['To'] = recipient
+    msg['From'] = sender
+
+    part = MIMEText('text', "html")
+    part.set_payload(message)
+    msg.attach(part)
+
+    session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+
+    session.ehlo()
+    session.starttls()
+    session.ehlo
+    session.login(sender, password)
+
+    session.sendmail(sender, recipient, msg.as_string())
+    session.quit()
+
+if __name__ == '__main__':
+    recipient = 'rk@dropbox.com, mskeving@gmail.com'
+    send_email_to(recipient)
+
