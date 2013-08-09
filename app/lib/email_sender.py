2a2e4b8215599b29db3ecc15984df8f8c6978ce0
Ryan Kaplan <ryankap@gmail.com>
2013-08-08 17:19:25 -0700

    Make the emails a little nicer

--- app/lib/email_sender.py
+++ app/lib/email_sender.py
@@ -5,37 +5,33 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+import email_string
+
+
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
 
 sender = 'bobmahoney1234@gmail.com'
 password = 'norfolkislandpine'
 subject = 'Someone sent you kudos!'
-message = """
-<html>
-    <body>
-        <div class="container" style="background-color: #4688f1; color: white; border-radius: 3px; padding: 15px; text-align: center;">
 
-            <div class="kudos header" style="font-size: 25px;">
-                Someone sent you Kudos...
-            </div>
+def send_email(link, message, recipient):
+    """
+    Sends an email with a button in it
 
-            <div class="kudos message" style="width: 320px; margin: 15px auto auto auto; color: white;">
-                Soleio is a cool guy! He will make this email look way more awesome than it is right now.
-                At the very least, it is currently way too blue and the font is the default ugly GMail font.
-            </div>
-    </body>
-</html>
-"""
+    @param - link - the link that the button takes the user to
+    @param - message - the message that the kudos-giver attaches to the post
+    @param - recipient - an email address, or comma-separated list of email addresses
+    """
+    email_body = email_string.gen_email_string(link, message)
 
-def send_email_to(recipient):
     msg = MIMEMultipart()
     msg['Subject'] = subject
     msg['To'] = recipient
     msg['From'] = sender
 
     part = MIMEText('text', "html")
-    part.set_payload(message)
+    part.set_payload(email_body)
     msg.attach(part)
 
     session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
@@ -50,5 +46,5 @@ def send_email_to(recipient):
 
 if __name__ == '__main__':
     recipient = 'rk@dropbox.com, mskeving@gmail.com'
-    send_email_to(recipient)
+    send_email('www.google.com', 'This is a cool message!', recipient)
 
