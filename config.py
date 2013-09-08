605bf8d4966118700b0c63404faaaaf8946c0022
missy <mskeving@gmail.com>
2013-09-07 18:26:48 -0700

    heroku

--- config.py
+++ config.py
@@ -4,7 +4,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSRF_ENABLED = True #CROSS-SITE REQUEST FORGERY
 SECRET_KEY = 'you-will-never-guess'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 #URI is required fy flask-sqlalchemy extension. This is path to database file
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 #folder where we store sqlalchemy migrate data files
