4ffa3f1a11776bd8e59b914b1ff5f9c751ad827d
missy <mskeving@gmail.com>
2013-09-26 19:56:24 -0700

    S3 connection works

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ CSRF_ENABLED = True #CROSS-SITE REQUEST FORGERY
 SECRET_KEY = 'you-will-never-guess'
 
 try:
-	raise Exception('No AWS')
 	AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 	AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 	S3_BUCKET = os.environ.get('S3_BUCKET')
