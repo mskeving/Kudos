135f6b20c745b2f2c325317aa06ecccb870de429
Daniel Eden <dan.eden@me.com>
2013-11-06 17:17:08 -0800

    Start the design migration.
    Add grunt stuff for automated Sass compiling, begin massacring HTML.
    Gulp.

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+	pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig({
+		// Sass
+		sass: {
+		  dist: {
+		  	options: {
+		  		sourcemap: 'true'
+		  	},
+		    files: {
+		    	// 'output.css' : 'input.scss'
+		        'app/static/css/style.css' : 'app/static/sass/style.scss'
+		    }
+		  }
+		},
+		// Auto-prefix CSS properties using Can I Use?
+	  autoprefixer: {
+	    options: {
+	    	// Last 2 versions of all browsers, plus IE7/8, BB10 (LOL), and Android 3+
+	    	browsers: ['last 2 versions', 'ie 8', 'ie 7', 'bb 10', 'android 3']
+	    },
+	    no_dest: {
+	    	// File to output
+	      src: 'app/static/css/style.css',
+	    },
+	  },
+	  csso: {
+      dist: {
+        files: {
+        	// Output compressed CSS to style.min.css
+          'app/static/css/style.min.css': ['app/static/css/style.css']
+        }
+      }
+    },
+    // Watch files for changes
+    watch: {
+      css: {
+        files: ['app/static/sass/style.scss'],
+        // Run Sass, autoprefixer, and CSSO
+        tasks: ['sass', 'autoprefixer', 'csso'],
+        options: {
+          spawn: false,
+        },
+      },
+    }
+	});
+
+	// Register our tasks
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-csso');
+	grunt.registerTask('default', ['watch']);
+};
